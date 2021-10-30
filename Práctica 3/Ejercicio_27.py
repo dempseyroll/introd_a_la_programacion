3c0a8bc501c183b6732180dce49d90f3449314fe
dempseyroll <71336897+dempseyroll@users.noreply.github.com>
2021-10-29 23:55:19 -0300

    Corrección de este ejercicio

--- Práctica 3/Ejercicio_27.py
+++ Práctica 3/Ejercicio_27.py
@@ -4,23 +4,18 @@
 
 palabra = input("Ingrese una palabra por favor: ") 
 vocales = "aeiou"
-primer_vocal = ""
-anterior=""
+anterior=" "
 diptongo = 0
 
-# Prueba: laura
-
 for i in palabra:
-    if i in vocales:
-        primer_vocal = i
-        if anterior in vocales and i == primer_vocal:
-            diptongo += 1
-            break        
+    if anterior in vocales and (i in vocales):
+        diptongo += 1
+        break        
     anterior = i
 
 print("La palabra",palabra,"tiene",diptongo,"diptongo.")
 
-## En realidad quedó desprolijo, porque no estaría contemplando los casos que tengan más de un diptongo. Me hubiera gustado
-## meterle esa complejidad.
-## Me falla con las palabras que empiezan con una vocal y no tienen realmente diptongo.
-## Por ejemplo: astro, alejandro.
+
+# Pruebas diptongo: laura, leer, astronauta, paula, florencia,
+# Pruebas sin diptongo: astro, alejandro, jarro, taza, elefante
+# éxito!
